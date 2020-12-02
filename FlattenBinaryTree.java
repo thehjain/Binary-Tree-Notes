e62406335d9bd74e23647e81590d14943bfd46b5
Harsh <thehjain@gmail.com>
2020-12-02 10:25:49 +0530

    Flatten Binary Tree to LinkedList

--- FlattenBinaryTree.java
+++ FlattenBinaryTree.java
@@ -21,5 +21,21 @@ public void flatten(TreeNode root) {
 }
 
 
+//Fastest approach using kind of Morris Traversal
+public void flatten(TreeNode root) {
+	TreeNode cur = root;
+	while (cur != null) {
+		if (cur.left != null) {
+			TreeNode last = cur.left;
+			while (last.right != null) last = last.right;
+			last.right = cur.right;
+			cur.right = cur.left;
+			cur.left = null;
+		}
+		cur = cur.right;
+	}
+}
+
+
 
 //Problem is available on leetcode
